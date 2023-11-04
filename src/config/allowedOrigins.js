4ffcc32f3a43ecfa244cdaa7069d1985ab9cbf25
hamzakamran <hamzakwebdev@gmail.com>
2023-11-03 22:18:04 -0500

    CORS

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:5000", "http://localhost:5001"];
+const allowedOrigins = [
+	"http://localhost:5000",
+	"http://localhost:5001",
+	"https://kahf.netlify.app",
+];
 
 module.exports = allowedOrigins;
